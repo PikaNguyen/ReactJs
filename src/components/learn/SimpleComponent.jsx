3a76502e61c197f2589a5cd86453088b8ec60c3c
Pika <nguyenvannhut0704@gmail.com>
2024-07-30 21:52:50 +0700

    variable Reactjs

--- src/components/learn/SimpleComponent.jsx
+++ src/components/learn/SimpleComponent.jsx
@@ -3,16 +3,33 @@
 import './style.css'
 
 const MyComponent = () => {
+    const number = 553750123;
+    const boolean = false; //thường k hiển thị vì k có ý nghĩa
+    const undf = undefined // same upon
+    const nil = null //same upon
+
+    // tham chiếu
+
+    const arr = [1, 5, 8] //array
+    const student = {
+        name: "Khabanh",
+        age: 18
+    }
     return (
         //Fragment mảnh vỡ, nhỏ
         <>
             <div className="divClass">
-                OOi ban oi - ReactJs
+                OOi ban oi - ReactJs {number}
                 <div className="childDiv"
                     style={
                         { borderRadius: "50px" }
                     }
-                >Content with color</div>
+                >Content {boolean}with color</div>
+                Array : {arr}
+
+                Student's name is {student.name}
+                <div></div>
+                Full information: Json {JSON.stringify(student)}
             </div>
         </>
 

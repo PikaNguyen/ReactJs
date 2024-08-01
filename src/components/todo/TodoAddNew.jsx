75c6c9e9b10c7482684458f9c503b3ab4bbdc62b
Pika <nguyenvannhut0704@gmail.com>
2024-08-01 22:02:06 +0700

    using events: click, onChange, pass param

--- src/components/todo/TodoAddNew.jsx
+++ src/components/todo/TodoAddNew.jsx
@@ -3,11 +3,25 @@
 const TodoAddNew = (props) => {
     console.log(">>Check props Fuction: ", props)
     const { addNewTodo } = props
-    addNewTodo("PARAM Input form user")
+    //addNewTodo("PARAM Input form user")
+
+    const handleClick = () => {
+        alert("Click mee")
+    }
+    const printHandleOnChange = (name) => {
+        console.log("Type input", name)
+    }
     return (
         <div>
-            <input type="text" placeholder='Enter your task' />
-            <button className='button-add'>Add</button>
+            <input type="text"
+                //want to pass param into func --> Need to using arrow func
+                onChange={(event) => printHandleOnChange(event.target.value)}
+
+                placeholder='Enter your task' />
+            <button className='button-add'
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }

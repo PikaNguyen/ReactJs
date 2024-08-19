46d0e74dd1958267236e16d25b2d5046d64b97ad
Pika <nguyenvannhut0704@gmail.com>
2024-08-19 20:44:17 +0700

    using State onClick

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,28 +1,56 @@
 import { Button, Input } from "antd"
+import { useState } from "react"
 
 
 const UserForm = () => {
+
+    const [fullName, setFullName] = useState()
+    const [password, setPassword] = useState()
+    const [email, setEmail] = useState()
+    const [phoneNumber, setPhoneNumber] = useState()
+
+
+    const handleClickButton = () => {
+        console.log(">>>Check info Name:", { fullName, email, password })
+    }
+
     return (
         <div className="userForm" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "17px", flexDirection: "column" }}>
                 <div>
                     <span>Full Name</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Phone Number</span>
-                    <Input />
+                    <Input
+                        value={phoneNumber}
+                        onChange={(event) => { setPhoneNumber(event.target.value) }}
+                    />
                 </div>
                 <div>
-                    <Button type="primary">Submit</Button>
+                    <Button type="primary"
+                        onClick={handleClickButton}
+                    //onClick={() => { handleClickButton }}
+
+                    >Submit</Button>
                 </div>
             </div>
         </div>

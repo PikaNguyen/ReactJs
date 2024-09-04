63727d9b9d9fb44f2da6179f38e5c42572f7e896
Pika <nguyenvannhut0704@gmail.com>
2024-09-04 20:38:57 +0700

    implement Upload img

--- src/components/user/user.detail.jsx
+++ src/components/user/user.detail.jsx
@@ -1,10 +1,16 @@
-import { Drawer } from 'antd';
+import { Button, Drawer } from 'antd';
 
 const UserDetail = (props) => {
     const { dataUserDetail, setDataUserDetail, setOpenDataUserDetail, isOpenUserDetail } = props
+    const UploadImage = () => {
+
+    }
+
     return (
 
-        <Drawer title="Detail user"
+        <Drawer
+            width={"40vw"}
+            title="Detail user"
             onClose={() => {
                 setDataUserDetail(null)
                 setOpenDataUserDetail(false)
@@ -18,7 +24,33 @@ const UserDetail = (props) => {
                 <p>Email: {dataUserDetail.email}</p>
                 <br />
                 <p>Phone number: {dataUserDetail.phone}</p>
+                <br />
+                <p></p>
+                <div>
+                    <img style={{
+                        width: "100px",
+                        height: "auto"
+                    }} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUserDetail.avatar}`} />
+                </div>
+                <div>
+                    <label htmlFor='btnUpload'
+                        style={{
+                            display: "block",
+                            width: "fit-content",
+                            borderRadius: "10px",
+                            cursor: "pointer",
+                            marginTop: "10px",
+                            padding: "5px 10px",
+                            background: "green"
+                        }}
+                    >
+                        Upload file</label>
+                    <input type='file' hidden id='btnUpload' />
+                </div>
 
+
+
+                <Button type='primary'>Upload/Update image</Button>
             </>
                 :
                 <>
